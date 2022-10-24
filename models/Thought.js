d107fe911034fb76ad05b94cbb99415e9916c7f2
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-24 11:44:26 -0700

    work on models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    published: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    text: {
+      type: String,
+      minLength: 15,
+      maxLength: 500,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `tagCount` that gets the amount of comments per user
+thoughtSchema
+  .virtual('tagCount')
+  // Getter
+  .get(function () {
+    return this.tags.length;
+  });
+
+// Initialize our Post model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
