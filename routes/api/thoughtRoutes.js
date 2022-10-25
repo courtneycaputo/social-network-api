aa898083c2291a437b095c79268a704ed5804c29
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-25 08:16:42 -0700

    finish thought and reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -32,16 +32,84 @@ router.get('/', async (req, res) => {
 })
 
 // GET one thought by ID at api/thoughts/:id
+router.get('/:thoughtId', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v');
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: "Invalid thought ID!" })
+        }
+        res.status(200).json(dbThoughtData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // UPDATE thought by ID at api/thoughts/:id
-
+router.put('/:thoughtId', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId }, 
+            {$set: req.body}, 
+            {new: true, runValidators: true})            
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: "Invalid thought ID!" })
+        }
+        res.status(200).json(dbThoughtData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // DELETE thoughts by ID at api/thoughts/:id
-
+router.delete('/:thoughtId', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.findOneAndDelete({ _id: req.params.thoughtId })          
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: "Invalid thought ID!" })
+        }
+        res.status(200).json(dbThoughtData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // ADD REACTION to thought
-
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const dbReactionData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId }, 
+            {$addToSet: { reactions: req.body}}, 
+            {new: true}
+        )
+        .populate('reactions')
+        .select('-__v');
+        if (!dbReactionData) {
+            return res.status(404).json({ message: "Invalid thought ID!" })
+        }
+        res.status(200).json(dbReactionData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 // DELETE REACTION to thought
+router.delete('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const dbReactionData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId }, 
+            {$pull: { reactions: req.body}}, 
+            {new: true}
+        )
+        .populate('reactions')
+        .select('-__v');
+        if (!dbReactionData) {
+            return res.status(404).json({ message: "Invalid thought ID!" })
+        }
+        res.status(200).json(dbReactionData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
