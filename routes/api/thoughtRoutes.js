2619cfe2e16fb0b9d2b304b76fecf04327b31093
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-10-24 17:47:39 -0700

    create thoughts get and create routes and outline remaining thought routes needed

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,20 +1,47 @@
 const router = require('express').Router();
 const { Thought, User } = require('../../models')
- 
-// //api/thoughts
-// router.get('/', (req,res) => {
+
+// CREATE thought at /api/thoughts
+router.post('/', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.create(req.body);
+        const dbUserData = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $push: { thoughts: dbThoughtData._id } },
+            { new: true }
+        );
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Thought created but invalid user ID!" })
+        }
+        res.status(200).json(dbThoughtData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET ALL thoughts at /api/thoughts
+router.get('/', async (req, res) => {
+    try {
+        const dbThoughtData = await Thought.find()
+            .sort({ createdAt: -1 })
+            .select('-__v');
+        res.status(200).json(dbThoughtData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET one thought by ID at api/thoughts/:id
+
+// UPDATE thought by ID at api/thoughts/:id
 
 
-// })
+// DELETE thoughts by ID at api/thoughts/:id
 
-// router.post('/', async (req, res) => {
-//     try{
-//         const dbThoughtData = await Thought.create(req.body);
-//         res.status(200).json(dbThoughtData);
-//     } catch(err) {
-//         res.status(500).json(err)
-//     }
-// })
 
+// ADD REACTION to thought
+
+
+// DELETE REACTION to thought
 
 module.exports = router;
